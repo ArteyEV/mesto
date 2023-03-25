900094547ff704beab580890e84bec5ed333b3ec
Artey <ivan.arteew@yandex.ru>
2023-03-26 00:05:55 +0300

    ok

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,7 @@ const profileButtonEdit = profile.querySelector('.profile__button-edit');
 const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
 // Elements variables
-const elements = document.querySelector('.elements');
-const cardButtons = elements.querySelectorAll('.card__button');
+
 // Popup variables
 const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup__button-close');
@@ -13,22 +12,8 @@ const popupForm = popup.querySelector('.popup__form');
 const nameInput = popup.querySelector('.popup__input_type_name');
 const jobInput = popup.querySelector('.popup__input_type_job');
 
-// Elements functions
-cardButtons.forEach(function(cardButton) {
-  cardButton.addEventListener('click', function() {
-    cardButton.classList.toggle('card__button_is-active');
-  });
-});
-
-// Popup functions
-function popupEnabled() {
-  popup.classList.add('popup_is-opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-}
-
 function popupDisabled() {
-  popup.classList.remove('popup_is-opened');
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
@@ -41,3 +26,10 @@ function handleFormSubmit (evt) {
 profileButtonEdit.addEventListener('click', popupEnabled);
 buttonClose.addEventListener('click', popupDisabled);
 popupForm.addEventListener('submit', handleFormSubmit);
+
+// Popup functions
+function popupEnabled() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
