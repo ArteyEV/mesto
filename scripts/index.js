6a30fbb9d0ef08ef0a7c7b98b8464a9d155ebcc8
Artey <ivan.arteew@yandex.ru>
2023-04-18 19:38:00 +0300

    18

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,106 @@
 // Profile variables
 const profile = document.querySelector('.profile');
 const profileButtonEdit = profile.querySelector('.profile__button-edit');
+const profileButtonAdd = profile.querySelector('.profile__button-add');
 const profileTitle = profile.querySelector('.profile__title');
 const profileSubtitle = profile.querySelector('.profile__subtitle');
-// Elements variables
+
+// Gallery variables
+const gallery = document.querySelector('.gallery');
+const galleryList = gallery.querySelector('.gallery__list');
 
 // Popup variables
-const popup = document.querySelector('.popup');
-const buttonClose = popup.querySelector('.popup__button-close');
-const popupForm = popup.querySelector('.popup__form');
-const nameInput = popup.querySelector('.popup__input_type_name');
-const jobInput = popup.querySelector('.popup__input_type_job');
+// Popup Edit
+const popupEdit = document.querySelector('.popup-edit');
+const popupEditButtonClose = popupEdit.querySelector('.popup__button-close');
+const popupEditForm = popupEdit.querySelector('.popup__form');
+const popupEditNameInput = popupEdit.querySelector('.popup__input[name="name"]');
+const popupEditJobInput = popupEdit.querySelector('.popup__input[name="job"]');
+// Popup Add
+const popupAdd = document.querySelector('.popup-add');
+const popupAddButtonClose = popupAdd.querySelector('.popup__button-close');
+const popupAddForm = popupAdd.querySelector('.popup__form');
+const popupAddPlaceNameInput = popupAdd.querySelector('.popup__input[name="place-name"]');
+const popupAddLinkInput = popupAdd.querySelector('.popup__input[name="link"]');
+// Popup Picture
+const popupPicture = document.querySelector('.popup-picture');
+const popupPictureButtonClose = popupPicture.querySelector('.popup__button-close');
+const popupFigcaption = popupPicture.querySelector('.popup__figcaption');
+const popupBigImage = popupPicture.querySelector('.popup__big-image');
 
-function popupDisabled() {
+// Card Template
+const cardTemplate = document.querySelector('#card-template').content;
+
+// Popup functions
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup (popup) {
   popup.classList.remove('popup_opened');
 }
 
-function handleFormSubmit (evt) {
+// PopupEdit functions
+function enableEditPopup() {
+  openPopup(popupEdit);
+  popupEditNameInput.value = profileTitle.textContent;
+  popupEditJobInput.value = profileSubtitle.textContent;
+};
+
+function handleEditFormSubmit (evt) {
   evt.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
-  popupDisabled();
-}
+  profileTitle.textContent = popupEditNameInput.value;
+  profileSubtitle.textContent = popupEditJobInput.value;
+  closePopup(popupEdit);
+};
 
-profileButtonEdit.addEventListener('click', popupEnabled);
-buttonClose.addEventListener('click', popupDisabled);
-popupForm.addEventListener('submit', handleFormSubmit);
+profileButtonEdit.addEventListener('click', enableEditPopup);
+popupEditButtonClose.addEventListener('click', () => closePopup(popupEdit));
+popupEditForm.addEventListener('submit', handleEditFormSubmit);
 
-// Popup functions
-function popupEnabled() {
-  popup.classList.add('popup_opened');
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-}
+// PopupPicture functions
+popupPictureButtonClose.addEventListener('click', () => closePopup(popupPicture));
+
+// Function add card
+function createCard(name, link) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+  // Функция удаления карточки
+  card.querySelector('.card__delete-button').addEventListener('click', () => {
+    card.remove();
+  });
+  // Функция лайка карточки
+  card.querySelector('.card__button').addEventListener('click', evt => {
+    evt.target.classList.toggle('card__button_is-active');
+  });
+  // Функция открытия попапа с картинкой
+  cardImage.addEventListener('click', () => {
+    openPopup(popupPicture);
+    popupBigImage.src = cardImage.src;
+    popupBigImage.alt = cardImage.alt;
+    popupFigcaption.textContent = cardTitle.textContent;
+  });
+  return card;
+};
+
+// Добавление начальных карточек из массива
+initialCards.forEach(item => {
+  galleryList.append(createCard(item.name, item.link));
+});
+
+// PopupAdd functions
+function handleAddFormSubmit (evt) {
+  evt.preventDefault();
+  // Добавление новой карточки
+  galleryList.prepend(createCard(popupAddPlaceNameInput.value, popupAddLinkInput.value));
+  closePopup(popupAdd);
+  popupAddForm.reset();
+};
+
+profileButtonAdd.addEventListener('click', () => openPopup(popupAdd));
+popupAddButtonClose.addEventListener('click', () => closePopup(popupAdd));
+popupAddForm.addEventListener('submit', handleAddFormSubmit);
